c7617171ca1cdf4fa4c21cb11b31b0c0fd6044a4
Andrey Pogorelov <namtydapro@gmail.com>
2020-03-12 00:24:55 +0300

    Add: Bonus task, README

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 Решение в index.html
 Промисифицирую методы API
 
+Бонусные задачи, в *bonusTask.js*
+
 Вызывать из консоли как-то так
 ```
 sumOdd(new AsyncArray([1, 2, 2, 2, 2, 2, 5]), (res) => console.log(res))
 ```
+[gh-pages](https://namtyda.github.io/hw2-shri-async/index.html)
+
+Работу можно показывать на разборе
--- bonusTask.js
+++ bonusTask.js
@@ -1,12 +1,26 @@
-const p1 = new Promise((resolve, reject) => setTimeout(reject, 1000, 1));
-const p2 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'err 2'));
-const p3 = new Promise((resolve) => setTimeout(resolve, 1500, 3));
+Promise._any = array => {
+  const result = [];
+  let counter = 0; ы
+
+  return new Promise((resolve, _) => {
+    array.forEach((prom, i) => prom
+      .then(
+        value => resolve(value),
+        reason => result[i] = ({ status: 'rejected', reason }))
+      .then(() => {
+        counter++;
+        if (counter === array.length) {
+          resolve(result);
+        }
+      }));
+  });
+}
 
 Promise._allSettled = array => {
   const result = [];
   let counter = 0;
-  
-  return new Promise((resolve, reject) => {
+
+  return new Promise((resolve, _) => {
     array.forEach((prom, i) => prom
       .then(
         value => result[i] = ({ status: 'fulfilled', value }),
@@ -20,7 +34,8 @@ Promise._allSettled = array => {
   });
 }
 
-const prom = [p1, p2, p3];
-
-Promise._allSettled([p1, p2, p3]).
-  then(el => el.forEach(item => console.log(item)))
+Promise._finally = fn => {
+  const fn2 = () => this;
+  const prom = () => Promise.resolve(fn()).then(fn2);
+  return this.then(prom, prom);
+};
